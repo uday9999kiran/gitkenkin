763a74025b4d078344f4493bace9d7cc4b72b8c8
uday9999kiran <60955975+uday9999kiran@users.noreply.github.com>
2020-02-12 09:45:46 +0000

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -31,7 +31,7 @@ public class Stepdefs {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.get("http://10.232.237.143:443/TestMeApp/login.htm");
+		driver.get("http://172.31.36.202:443/TestMeApp/fetchcat.htm");
 		
 	} 
 
